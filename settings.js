43233961e0035d4174204648bd84037d6bb2cd8f
Torcher01 <miskichekov@gmail.com>
2024-02-10 14:08:37 +0700

    Добавляет проект в репозиторий

---
+++ settings.js
@@ -0,0 +1,15 @@
+const GAME_TYPES = [
+  { type: 'cats', text: 'Котики' },
+  { type: 'flowers', text: 'Цветочки' },
+  { type: 'cars', text: 'Машины' },
+];
+
+const AppRoute = {
+  Initial: 'initial',
+  Game: 'game',
+  Results: 'results',
+};
+
+const TIMEOUT = 900;
+
+const RANDOMIZED = false;
